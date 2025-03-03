f2d5422a3555162c11294ab65193cf2c739de6d0
Pedro Lorenzo Rosario <senpai@Pedros-MacBook-Pro.local>
2025-03-02 21:14:55 -0400

    dded hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import HomeView from './views/HomeView'
 import MovieDetails from './views/MovieDetailView'
 import './App.css'
